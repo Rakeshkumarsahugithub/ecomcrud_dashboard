43a3fb271d1dd9c83e5509f9d90460cec4afdb3e
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-02-15 01:03:07 +0530

    Update Productlist.jsx

--- client/src/Productlist.jsx
+++ client/src/Productlist.jsx
@@ -12,7 +12,7 @@ const ProductList = () => {
 
     const fetchProducts = async () => {
         try {
-            const result = await axios.get("http://localhost:5000/products", { withCredentials: true });
+            const result = await axios.get("https://ecomcrud-dashboard.onrender.com/products", { withCredentials: true });
             setProducts(result.data);
         } catch (error) {
             console.error("Error fetching products:", error);
@@ -21,7 +21,7 @@ const ProductList = () => {
 
     const deleteProduct = async (id) => {
         try {
-            await axios.delete(`http://localhost:5000/products/${id}`, { withCredentials: true });
+            await axios.delete(`https://ecomcrud-dashboard.onrender.com/products/${id}`, { withCredentials: true });
             fetchProducts();
         } catch (error) {
             console.error("Error deleting product:", error);
@@ -43,7 +43,7 @@ const ProductList = () => {
     const searchHandle = async () => {
         if (searchKey) {
           try {
-            const result = await axios.get(`http://localhost:5000/search/${searchKey}`, { withCredentials: true });
+            const result = await axios.get(`https://ecomcrud-dashboard.onrender.com/search/${searchKey}`, { withCredentials: true });
             setProducts(result.data.data); // Access "data" from API response
           } catch (error) {
             console.error("Error searching products:", error);
