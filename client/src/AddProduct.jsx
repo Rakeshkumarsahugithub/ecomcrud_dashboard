6f6eb11a24ad9e9f2e20757c57b84d80beb48240
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-02-15 14:01:54 +0530

    Update AddProduct.jsx

--- client/src/AddProduct.jsx
+++ client/src/AddProduct.jsx
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 const AddProduct = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
