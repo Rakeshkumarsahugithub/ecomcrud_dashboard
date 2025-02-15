77d6dca1dee05476e23ab7fdcbd8b72bfe33a7ab
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-02-15 19:59:10 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default {
+  build: {
+    rollupOptions: {
+      external: ['bootstrap/dist/css/bootstrap.min.css']
+    }
+  }
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
