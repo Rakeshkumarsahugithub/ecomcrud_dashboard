42ef3365b2bcdc13efbc8902b5848b8829a3d505
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-02-15 21:05:31 +0530

    Update Nav.jsx

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -1,44 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Nav = ({ isAuthenticated, handleLogout }) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <Link to="/" className="navbar-brand">E-Commerce</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        {!isAuthenticated ? (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/signup" className="nav-link">Sign Up</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/login" className="nav-link">Log In</Link>
-                                </li>
-                            </>
-                        ) : (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/home" className="nav-link">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/products" className="nav-link">Products</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/add-product" className="nav-link">Add Product</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </div>
-            </div>
+        <nav>
+            {!isAuthenticated ? (
+                <ul className="navul right">
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to="/login">Log In</Link></li>
+                </ul>
+            ) : (
+                <ul className="navul">
+                    <li><Link to="/home" className="home-link">Home</Link></li>
+                    <li><Link to="/products" className="home-link">Products</Link></li>
+                    <li><Link to="/add-product" className="home-link">Add Product</Link></li>
+                    <li>
+                        <button className="Logbtn" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </li>
+                </ul>
+            )}
         </nav>
     );
 };
